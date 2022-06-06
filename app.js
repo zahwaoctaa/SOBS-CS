5292efe06faf425cbff86a3a1d11d68c9f3c533b
Khoirunnisa <khoirunnisaputri92@gmail.com>
2022-06-06 18:46:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -40,6 +40,23 @@ app.post('/api/register_user', (req, res) => {
     });
 
 });
+app.post('/api/transaction', (req, res) => {
+    // buat variabel penampung data dan query sql
+    const data = { ...req.body };
+    const querySql = 'SELECT * FROM transaction';
+
+    // jalankan query
+    koneksi.query(querySql, data, (err, rows, field) => {
+        // error handling
+        if (err) {
+            return res.status(500).json({ message: 'Gagal insert data!', error: err });
+        }
+
+        // jika request berhasil
+        res.status(201).json({ success: true, message: 'Berhasil insert data!' });
+    });
+
+});
 
 // read data / get data
 app.get('/api/category', (req, res) => {
